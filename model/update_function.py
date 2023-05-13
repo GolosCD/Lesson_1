5e7049bb2257526be30a28359f7522528d351684
GolosCD <ananev@asta>
2023-05-13 20:32:20 +0300

    elf

--- model/update_function.py
+++ model/update_function.py
@@ -20,22 +20,3 @@ def upd_gold_player(qty_gold,type_operation):
         value['user_resurce']['gold_float'] =\
             get_current_qty_gold() + qty_gold
 
-# ----------upd_qty_build-----------------------------------------
-# функция зменяет кол-ва построек выбранного типа
-# в функцию передается название постройки и кол-во и знак + или -
-# на которое надо увеличить текущий показатель
-def upd_qty_build(build_name, qty, mark):
-        if mark == '+': # если + то операция увеличения кол-ва зданий
-                value[build_name]['count_int'] =\
-                    value.get(build_name).get('count_int') + qty
-        elif mark == '-': # если минус, то операция уменьшеня кол-ва зданий
-                if get_current_qty_build(build_name) >= qty:
-                        value[build_name]['count_int'] =\
-                            value.get(build_name).get('count_int') - qty
-                else: # если зданий меньше чем мы хотим уменьшить то просто 0
-                        value[build_name]['count_int'] = 0
-        else:
-                try:
-                        print('Invalid input mark')
-                except KeyError:
-                        print('Invalid input mark, please try again.\n')
